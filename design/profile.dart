34723c353acb4a119badfe8b8eccbc9ccd61f46c
m0dern6 <na.ab.en.153@gmail.com>
2024-05-01 22:51:00 +0545

    minor correction

--- design/profile.dart
+++ design/profile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:service_provide_app/login_signup/login_screen.dart';
+import 'package:service_pro_provider/Ui/login_signup/login_screen.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
