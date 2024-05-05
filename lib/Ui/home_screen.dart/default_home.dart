d0fc8f602db74bf4fe80a37816dea3a915730915
m0dern6 <na.ab.en.153@gmail.com>
2024-05-05 21:34:30 +0545

    bottom navigation bar added

---
+++ lib/Ui/home_screen.dart/default_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DefaultHome extends StatefulWidget {
+  const DefaultHome({super.key});
+
+  @override
+  State<DefaultHome> createState() => _DefaultHomeState();
+}
+
+class _DefaultHomeState extends State<DefaultHome> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(),
+    );
+  }
+}
--- lib/Ui/home_screen.dart/home_screen.dart
+++ lib/Ui/home_screen.dart/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:service_pro_provider/Ui/chat_screen/chat_screen.dart';
+import 'package:service_pro_provider/Ui/home_screen.dart/default_home.dart';
+import 'package:service_pro_provider/Ui/profile/profile_page.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,104 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  Color? unSelectedItemColor = Colors.pink[900];
+  Color selectedItemColor = Colors.white;
+  late Widget currentBody;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    switch (currentIndex) {
+      case 0:
+        currentBody = DefaultHome();
+
+        break;
+      case 1:
+        currentBody = Chat();
+
+        break;
+
+      case 2:
+        currentBody = ProfilePage();
+        break;
+    }
+    return SafeArea(
+        child: Scaffold(
+      // backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(5),
+            bottomRight: Radius.circular(5),
+          ),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            title: const Text('Service Pro'),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
+      body: currentBody,
+      bottomNavigationBar: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, left: 5, right: 5),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: BottomNavigationBar(
+                // type: BottomNavigationBarType.fixed,
+                backgroundColor: Theme.of(context).primaryColor,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icons/home.png',
+                      height: 30,
+                      color: currentIndex == 0
+                          ? selectedItemColor
+                          : unSelectedItemColor,
+                    ),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset('assets/icons/chat.png',
+                        height: 30,
+                        color: currentIndex == 1
+                            ? selectedItemColor
+                            : unSelectedItemColor),
+                    label: 'Chat',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset('assets/icons/profile.png',
+                        height: 30,
+                        color: currentIndex == 2
+                            ? selectedItemColor
+                            : unSelectedItemColor),
+                    label: 'Profile',
+                  ),
+                  // BottomNavigationBarItem(
+                  //   icon: Image.asset('assets/icons/profile.png',
+                  //       height: 30,
+                  //       color: currentIndex == 3
+                  //           ? selectedItemColor
+                  //           : unSelectedItemColor),
+                  //   label: 'Profile',
+                  // ),
+                ],
+                currentIndex: currentIndex,
+                selectedItemColor: selectedItemColor,
+                unselectedItemColor: unSelectedItemColor,
+                onTap: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
