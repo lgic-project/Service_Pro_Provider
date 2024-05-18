7de9a203bf3a4c5ea04b098e521ed44987d37eb0
m0dern6 <na.ab.en.153@gmail.com>
2024-05-18 08:05:48 +0545

    displayed chat user list

--- lib/Ui/chat/chat_list.dart
+++ lib/Ui/chat/chat_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:service_pro_user/Provider/chat_user_provider.dart';
+import 'package:service_pro_provider/Provider/chat_user_provider.dart';
 
 class Chat extends StatefulWidget {
   const Chat({super.key});
@@ -29,56 +29,56 @@ class _ChatState extends State<Chat> {
           ],
         ),
       ),
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            title: const Text('Chat'),
-          ),
-          body: Container(
-            padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-            child: Column(
-              children: [
-                const Text(
-                  'Your Service Providers...',
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                Expanded(
-                  child: Consumer<ChatUserProvider>(
-                      builder: (context, chatUser, child) {
-                    return ListView.builder(
-                        itemCount: chatUser.users.length,
-                        itemBuilder: (context, index) {
-                          final chatUsers = chatUser.users[index];
-                          if (chatUsers['Role'] == 'Provider') {
-                            return Column(
-                              children: [
-                                ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundImage: NetworkImage(''),
-                                  ),
-                                  title: Text(chatUsers['Name']),
-                                  trailing: const Text(
-                                    '5m',
-                                    style: TextStyle(color: Color(0xFF191645)),
-                                  ),
-                                ),
-                                const Divider(
-                                  color: Colors.white,
-                                ),
-                              ],
-                            );
-                          } else {
-                            return const SizedBox.shrink();
-                          }
-                        });
-                  }),
-                ),
-              ],
+      child: Container(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+        child: Column(
+          children: [
+            const Text(
+              'Your Service Providers...',
+              style: TextStyle(
+                fontSize: 18,
+              ),
             ),
-          )),
+            Expanded(
+              child: Consumer<ChatUserProvider>(
+                  builder: (context, chatUser, child) {
+                return ListView.builder(
+                    itemCount: chatUser.users.length,
+                    itemBuilder: (context, index) {
+                      final chatUsers = chatUser.users[index];
+                      if (chatUsers['Role'] == 'user') {
+                        return Column(
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  chatUsers['Image'].toString() != null
+                                      ? chatUsers['Image'].toString()
+                                      : AssetImage(
+                                              'assets/profile/default_profile.jpg')
+                                          .toString(),
+                                ),
+                              ),
+                              title: Text(chatUsers['Name']),
+                              trailing: const Text(
+                                '5m',
+                                style: TextStyle(color: Color(0xFF191645)),
+                              ),
+                            ),
+                            const Divider(
+                              color: Colors.white,
+                            ),
+                          ],
+                        );
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    });
+              }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:service_pro_provider/Provider/api_provider.dart';
-import 'package:service_pro_provider/Ui/home_screen.dart/home_screen.dart';
+import 'package:service_pro_provider/Provider/chat_user_provider.dart';
+import 'package:service_pro_provider/Provider/user_provider.dart';
+import 'package:service_pro_provider/Ui/navigator/navigator_scaffold.dart';
 import 'package:service_pro_provider/Ui/login_signup/login_screen.dart';
 import 'package:service_pro_provider/Ui/splash_screen/splash_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,16 +24,19 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => ApiProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => ChatUserProvider()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primaryColor: Colors.teal,
+          primaryColor: Color(0xFF42cbac),
         ),
         initialRoute: '/',
         routes: {
           '/': (context) => SplashScreen(),
-          '/dashboard': (context) => const HomeScreen(),
+          '/dashboard': (context) => const NavigatorScaffold(),
           '/login': (context) => const LoginScreen(),
         },
       ),
