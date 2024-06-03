9ead28a929de4ca506e27f6c2fb319ab517b8dd1
Sajeet07 <sajitgurung829@gmail.com>
2024-06-03 06:35:43 +0545

    updating login userinterface in service provider app

--- lib/Ui/login_signup/login_screen.dart
+++ lib/Ui/login_signup/login_screen.dart
@@ -1,175 +1,187 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:service_pro_provider/Provider/login_logout_provider.dart';
-import 'package:service_pro_provider/Ui/login_signup/signup.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  bool _isPasswordVisible = false;
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final apiProvider = Provider.of<LoginLogoutProvider>(context);
-
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Login'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/dashboard');
-              },
-              icon: Icon(Icons.close))
-        ],
-      ),
       body: Stack(
-        fit: StackFit.expand,
         children: [
+          // Background
           Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/loginimage.jpg"),
-                fit: BoxFit.cover,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.greenAccent, Color.fromARGB(255, 90, 251, 96)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+          // Bubble shapes
+          Positioned(
+            top: 50,
+            left: 30,
+            child: Bubble(color: Colors.blue, size: 60),
+          ),
+          Positioned(
+            top: 150,
+            right: 30,
+            child: Bubble(color: Colors.purple, size: 80),
+          ),
+          Positioned(
+            bottom: 100,
+            left: 50,
+            child: Bubble(color: Color.fromARGB(255, 240, 105, 226), size: 100),
+          ),
+          Positioned(
+            bottom: 200,
+            right: 50,
+            child: Bubble(color: Colors.pinkAccent, size: 60),
+          ),
+          // Login Form
+          Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              margin: EdgeInsets.symmetric(horizontal: 32),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 20,
+                    offset: Offset(0, 10),
+                  ),
+                ],
+              ),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  const SizedBox(height: 80),
-                  const Text(
-                    'SERVICE PRO',
-                    textAlign: TextAlign.center,
+                  Text(
+                    'Login',
                     style: TextStyle(
-                      color: Colors.amber,
+                      fontSize: 28,
                       fontWeight: FontWeight.bold,
-                      fontSize: 40,
-                      letterSpacing: 2,
+                      color: Colors.white,
                     ),
                   ),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      labelText: 'EMAIL ADDRESS',
-                      filled: true,
-                      fillColor: Colors.white,
-                      suffixIcon: Icon(Icons.email),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
-                    obscureText: !_isPasswordVisible,
+                  SizedBox(height: 20),
+                  TextField(
                     decoration: InputDecoration(
-                      labelText: 'PASSWORD',
+                      labelText: 'Username or email',
+                      labelStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(Icons.person, color: Colors.white),
                       filled: true,
-                      fillColor: Colors.white,
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
-                        icon: _isPasswordVisible
-                            ? const Icon(Icons.visibility)
-                            : const Icon(Icons.visibility_off),
+                      fillColor: Colors.white.withOpacity(0.2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
                       ),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
+                    style: TextStyle(color: Colors.white),
                   ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await apiProvider.login(
-                        _emailController.text,
-                        _passwordController.text,
-                      );
-
-                      // Check if the login was successful
-                      if (apiProvider.isLoggedIn) {
-                        Navigator.pushReplacementNamed(context, '/dashboard');
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text('Error'),
-                              content: const Text('Invalid email or password'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      }
-                    },
-                    child: apiProvider.isLoading
-                        ? const CircularProgressIndicator()
-                        : const Text('LOGIN'),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Don\'t have an account? ',
-                        style: TextStyle(color: Colors.black),
+                  SizedBox(height: 20),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.white),
+                      prefixIcon: Icon(Icons.lock, color: Colors.white),
+                      suffixIcon: Icon(Icons.visibility, color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
                       ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SignUpPage(),
-                            ),
-                          );
-                        },
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                            checkColor: Colors.blue,
+                            fillColor: MaterialStateProperty.all(Colors.white),
                           ),
+                          Text('Remember me',
+                              style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                              color: const Color.fromARGB(255, 241, 241, 239)),
                         ),
                       ),
                     ],
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.orange,
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Don't have an account? ",
+                        children: [
+                          TextSpan(
+                            text: 'Sign up',
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                        ],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Bubble extends StatelessWidget {
+  final Color color;
+  final double size;
+
+  Bubble({required this.color, required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.5),
+        shape: BoxShape.circle,
       ),
     );
   }
